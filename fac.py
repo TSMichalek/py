cf7a28dd132b94cb46c4fb9af749ea0e7e01655c
TSMichalek <mc_tmichalek26@mcjvs2.org>
2025-10-21 12:13:16 -0400

    Delete fac.py

--- fac.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Number: ")) #asks for a number
-product = 1     #the expresion is stated
-for i in range(1, number + 1): #does the factorial
-    product = product * i
-    
-
-print(f"The factorial of  {number} is {product}") #prints the results
