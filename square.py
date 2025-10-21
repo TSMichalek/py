4f4f7d8b692df82bbedce496664eed0a07352ea2
TSMichalek <mc_tmichalek26@mcjvs2.org>
2025-10-21 12:13:37 -0400

    Delete square.py

--- square.py
+++
@@ -1,8 +0,0 @@
-number = 1 #expresion
-
-for i in range(1,6): #range of 5
-    remainder = number * number #multiplies the number by itself
-    number += 1 #adds 1 to the number to repeat the prosess
-    print(remainder)
-
-    
