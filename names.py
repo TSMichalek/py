acd407438fb738d2b815b04070f97d1c05d7ceb8
TSMichalek <mc_tmichalek26@mcjvs2.org>
2025-10-21 12:13:27 -0400

    Delete names.py

--- names.py
+++
@@ -1,6 +0,0 @@
-name = str(input("Name: ")) #asks for name
-times = int(input("How many times? ")) #asks for value
-
-for i in range(times):
-    print(name, end=" ") #prints the name the specified amount, seperated by a space
-
